1d72e971063594605d17005f599f6760f212198e
OlgaGre <grechanyuko@gmail.com>
2022-09-22 13:46:35 +0300

    feat: setting webpack

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("ff")})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0LmpzIiwibWFwcGluZ3MiOiJtQkFBQUEsUUFBUUMsSUFBSSxLIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vaXRwcm9maXQvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ2ZmJykiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
